c1f926edd3aec66436b23bd05fcd82c20db79d80
abj <abj1.rts@gmail.com>
2020-10-08 21:33:46 +0530

    ShowImage option added

--- src/MainCompo.js
+++ src/MainCompo.js
@@ -1,5 +1,5 @@
 /**@jsx jsx */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Redirect, Route } from 'react-router-dom'
 import './App.css';
 
@@ -8,16 +8,21 @@ import { css, jsx } from '@emotion/core'
 
 import Header from './components/Header'
 import Center from './components/Center'
+import ShowImage from './components/ShowImage'
 
+import profile from './shared/Samal.jpg'
 
-function Home() {
+function Home(props) {
+
     return (
         <React.Fragment>
             <Container fluid="100%" style={{ backgroundColor: "#F9F8EB" }}>
                 <Header />
                 <Center />
+                {/* <ShowImage img={{profile, alt: "Gourav Samal"}}/> */}
             </Container>
-        </React.Fragment>
+
+        </React.Fragment >
     )
 }
 

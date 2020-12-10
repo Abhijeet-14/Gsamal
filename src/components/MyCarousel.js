1fd945e7f21720d0fd0d8ddd0a83b767b396bb93
abj <abj1.rts@gmail.com>
2020-12-10 19:32:42 +0530

    MyCarousel only 2 image

--- src/components/MyCarousel.js
+++ src/components/MyCarousel.js
@@ -6,32 +6,32 @@ import windowSize from 'react-window-size'
 import blog from '../shared/images/blog.png'
 
 import blog1_img0 from '../shared/b1_0.jpeg'
-import blog1_img1 from '../shared/b1_1.jpeg'
-import blog1_img2 from '../shared/b1_2.jpeg'
-import blog1_img3 from '../shared/b1_3.jpeg'
-import blog1_img4 from '../shared/b1_4.jpeg'
-import blog1_img5 from '../shared/b1_5.jpeg'
+// import blog1_img1 from '../shared/b1_1.jpeg'
+// import blog1_img2 from '../shared/b1_2.jpeg'
+// import blog1_img3 from '../shared/b1_3.jpeg'
+// import blog1_img4 from '../shared/b1_4.jpeg'
+// import blog1_img5 from '../shared/b1_5.jpeg'
 
-const images = [blog1_img0, blog1_img1, blog1_img2, blog1_img3, blog1_img4, blog1_img5]
+const images = [blog, blog1_img0]
 
 const titles = [
     "Slide 1",
     "Slide 2",
-    "Slide 3",
-    "Slide 4",
-    "Slide 5",
-    "Slide 6",
-    "Slide 1"
+    // "Slide 3",
+    // "Slide 4",
+    // "Slide 5",
+    // "Slide 6",
+    // "Slide 1"
 ]
 
 const bodies = [
     "This is my slide 1",
     "This is my slide 2",
-    "This is my slide 3",
-    "This is my slide 4",
-    "This is my slide 5",
-    "This is my slide 6",
-    "This is my slide 1"
+    // "This is my slide 3",
+    // "This is my slide 4",
+    // "This is my slide 5",
+    // "This is my slide 6",
+    // "This is my slide 1"
 ]
 
 
@@ -65,14 +65,13 @@ function MyCarousel({ windowWidth, windowHeight }) {
             <Row style={styles.Row}>
                 {size &&
                     <Col style={styles.Col} >
-                        <Image src={blog} style={{ width: "100%", height: "100%" }} />
+                        <Image src={images[index]} style={{ width: "100%", height: "100%" }} />
                     </Col>
                 }
                 <Col style={styles.Col}>
                     {!size &&
                         <Col style={styles.Col} className="mb-3">
-                            <Image src={blog} style={{ width: "100%", height: "100%" }} />
-                            {/* <Image src={images[index]} style={{ width: "100%", height: "100%" }} /> */}
+                            <Image src={images[index]} style={{ width: "100%", height: "100%" }} />
                         </Col>
                     }
                     <Card style={styles.Card}>
